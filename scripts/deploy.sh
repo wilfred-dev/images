4d1a3901d421ae94f793fbc3406d02a049d733a4
Vilhelm Prytz <vilhelm@prytznet.se>
2020-01-08 20:10:46 +0100

    Move docker push commands to seperate script

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/source-engine:latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/java:latest
