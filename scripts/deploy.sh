beb020568db4c771ce2c5986090e49a93fbe20c9
Vilhelm Prytz <vilhelm@prytznet.se>
2020-01-11 00:45:15 +0100

    Add Ubuntu based Docker image (add user)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/source-engine:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/java:latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/ubuntu:latest
