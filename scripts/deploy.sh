dd56cbb5d76c2bb40e5452f23ef8602407d722ef
Vilhelm Prytz <vilhelm@prytznet.se>
2020-01-13 22:33:40 +0100

    Add Debian base image & Multi Theft Auto

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/source-engine:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/java:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/ubuntu:latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push wilfreddev/debian:latest
